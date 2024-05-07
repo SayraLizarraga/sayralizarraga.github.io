0c3021c855cc0df2bb2802ad4605a5f9d124d9f1
SayraLizarraga <126122923+SayraLizarraga@users.noreply.github.com>
2024-05-06 19:43:03 -0600

    cambios2

--- arktos/src/app/app.component.ts
+++ arktos/src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-
 import { RouterLink, RouterOutlet,RouterLinkActive} from '@angular/router';
-
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,  RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
